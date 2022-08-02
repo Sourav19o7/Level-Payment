a914c65c3898355f0692daf6dc2124a2cf3a3797
Sourav <sourav.dey0147@gmail.com>
2022-08-02 22:57:45 +0530

    second commit

--- app/src/main/java/com/example/levelpayment/MainActivity.kt
+++ app/src/main/java/com/example/levelpayment/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<RelativeLayout>(R.id.homePage).setOnClickListener(){
+        findViewById<ConstraintLayout>(R.id.homePage).setOnClickListener(){
             //Log.i("No:","16")
             val intent = Intent(this, Payment::class.java)
             startActivity(intent)
