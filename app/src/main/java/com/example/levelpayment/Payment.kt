403622f51f3e39f554c5cb39c3c5588b840578d9
Sourav <sourav.dey0147@gmail.com>
2022-08-03 14:49:18 +0530

    Final Commit

--- app/src/main/java/com/example/levelpayment/Payment.kt
+++ app/src/main/java/com/example/levelpayment/Payment.kt
@@ -1,60 +1,64 @@
 package com.example.levelpayment
 
 import android.os.Bundle
-//import android.util.Log
 import android.view.View
-//import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.levelpayment.databinding.PaymentwindowBinding
 import com.google.firebase.database.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
-class Payment: AppCompatActivity() {
+class Payment : AppCompatActivity() {
     var firebaseDatabase: FirebaseDatabase? = null
 
     var databaseReference: DatabaseReference? = null
 
-    private lateinit var binding : PaymentwindowBinding
+    private lateinit var binding: PaymentwindowBinding
 
     private val paymentValue: PaymentValue = PaymentValue("100 $/yr")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.paymentwindow)
-        //Log.i("No:","20")
+
+        //Using data binding to connect layout and UI directly
         binding = DataBindingUtil.setContentView(this, R.layout.paymentwindow)
         binding.paymentValue = paymentValue
 
-        binding.idDiscount.setOnClickListener(){
-            GlobalScope.launch(Dispatchers.IO) {
+        //Using Coroutines for in-app messaging with firebase console
+        binding.idDiscount.setOnClickListener() {
+            runBlocking {
                 discount(it)
             }
         }
     }
 
-    suspend fun discount(view: View)
-    {
-        firebaseDatabase = FirebaseDatabase.getInstance()
 
-        databaseReference = firebaseDatabase!!.getReference("Data")
+    //discount() function is suspend, hence can only be called by another
+    //coroutine or suspend function
+    suspend fun discount(view: View) = coroutineScope {
+        launch {
+            firebaseDatabase = FirebaseDatabase.getInstance()
 
-        getdata(binding)
+            databaseReference = firebaseDatabase!!.getReference("Data")
+
+            getdata(binding)
+        }
     }
 }
 
-fun Payment.getdata(binding: PaymentwindowBinding){
+//getdata() is a extension function, a feature of Kotlin
+suspend fun Payment.getdata(binding: PaymentwindowBinding) = coroutineScope {
+    launch {
+        databaseReference!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val value = snapshot.getValue(String::class.java)
+                binding.idEdtAmt.setText(value)
+            }
 
-    databaseReference!!.addValueEventListener(object : ValueEventListener {
-        override fun onDataChange(snapshot: DataSnapshot) {
-            val value = snapshot.getValue(String::class.java)
-            binding.idEdtAmt.setText(value)
-        }
-        override fun onCancelled(error: DatabaseError) {
-            Toast.makeText(this@getdata, "Fail to get data.", Toast.LENGTH_SHORT).show()
-        }
-    })
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@getdata, "Fail to get data.", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
 }
